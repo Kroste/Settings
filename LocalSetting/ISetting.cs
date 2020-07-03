92cc0dfcbefc1f3b862ef3d8b2543cc55945aa1c
Kroste <40606143+Kroste@users.noreply.github.com>
2020-07-03 17:20:39 +0200

    Projektdateien hinzufügen.

---
+++ LocalSetting/ISetting.cs
@@ -0,0 +1,31 @@
+﻿namespace LocalSetting
+{
+  public enum Mode
+  {
+    Static,
+    Dynamic,
+    Expiring
+  }
+  public interface ISetting
+  {/// <summary>
+  /// 
+  /// </summary>
+  /// <returns>Exception Message or string.empty</returns>
+    string GetLastError();
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="Key"></param>Key of the Setting
+    /// <returns>Value</returns>
+    Setting GetSetting(string Key);
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="Key">Key of the Setting</param>
+    /// <param name="Value">Value of the Setting</param>
+    /// <param name="Mode">Caching Mode</param>
+    /// <param name="ExpirationTime">Only available when Mode = Expiring, Value in Second</param>
+    /// <returns></returns>
+    bool SetSetting(Setting Setting, Mode Mode = Mode.Static, int ExpirationTime = 10);
+  }
+}
---
+++ LocalSetting/LocalSetting/LocalSetting.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Configuration;
+using System.Runtime.Caching;
+
+namespace LocalSetting
+{
+  public class LocalSettings : ISetting
+  {
+    private string lastError = string.Empty;
+    public string GetLastError()
+    {
+      return lastError;
+    }
+
+    public Setting GetSetting(string Key)
+    {
+      //Laden der AppSettings
+      ObjectCache cache = MemoryCache.Default;
+
+      string val = (string)cache.Get(Key);
+
+      if (val != null)
+      {
+        return new Setting { Key = Key, Value = val };
+      }
+
+      CacheItemPolicy policy = new CacheItemPolicy
+      {
+        AbsoluteExpiration = DateTimeOffset.MaxValue
+      };
+
+      Configuration config = ConfigurationManager.OpenExeConfiguration(System.Reflection.Assembly.GetExecutingAssembly().Location);
+      val = config.AppSettings.Settings[Key]?.Value;
+      
+      if (val != null)
+        cache.Add(Key, val, policy);
+
+      //Zurückgeben der dem Key zugehörigen Value
+      return new Setting { Key = Key, Value = val };
+    }
+
+    public bool SetSetting(Setting Setting, Mode Mode = Mode.Static, int ExpirationTime = 10)
+    {
+      try
+      {
+        //Laden der AppSettings
+        Configuration config = ConfigurationManager.OpenExeConfiguration(
+                                System.Reflection.Assembly.GetExecutingAssembly().Location);
+        //Überprüfen ob Key existiert
+        if (config.AppSettings.Settings[Setting.Key] != null)
+        {
+          //Key existiert. Löschen des Keys zum "überschreiben"
+          config.AppSettings.Settings.Remove(Setting.Key);
+        }
+        //Anlegen eines neuen KeyValue-Paars        
+        config.AppSettings.Settings.Add(Setting.Key, Setting.Value);        
+        //Speichern der aktualisierten AppSettings
+        config.Save(ConfigurationSaveMode.Modified);
+        return true;
+      }
+#pragma warning disable CA1031 // Do not catch general exception types
+      catch (Exception ex)
+      {
+        lastError = ex.Message;
+        return false;
+      }
+#pragma warning restore CA1031 // Do not catch general exception types
+    }
+  }
+}
